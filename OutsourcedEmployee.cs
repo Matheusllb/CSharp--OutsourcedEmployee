13fa25491105d1f7a1e0271882b9710eb684fb66
Matheusllb <matheusluislopesbatista@gmail.com>
2023-06-06 06:04:18 -0300

    First commit

---
+++ OutsourcedEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace OutSourcedEmployee
+{
+    public class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharger { get; protected set; }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharger) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharger = additionalCharger;
+        }
+
+        public override double Payment()
+        {
+            return Hours * ValuePerHour + AdditionalCharger * 1.1;
+        }
+
+    }
+}
